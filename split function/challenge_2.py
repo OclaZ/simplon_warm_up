70278c1428e20a6f62ca2484bd89e621358cda0a
OclaZ <aslikhhamza2001@gmail.com>
2025-09-24 10:40:03 +0100

    challenge 2

---
+++ split function/challenge_2.py
@@ -0,0 +1,8 @@
+names=['hamza','sImo   ','    yassine','  ayouB','amine','zakaria']
+# fixed_names=[]
+# for name in names:
+#     fixed_names.append(name.strip().upper())
+# print(fixed_names)
+
+fixed_names=[name.strip().upper() for name in names]
+print(fixed_names)
